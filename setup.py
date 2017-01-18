86544cf6cbc097706926ba8133a2ad3baa5455e6
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2017-01-18 15:33:24 -0500

    created setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='RadFil',
+      version='0.1',
+      description='Build Radial Density Profiles for Interstellar Filaments',
+      url='http://github.com/catherinezucker/RadFil',
+      author='Catherine Zucker',
+      author_email='catherine.zucker@cfa.harvard.edu',
+      packages=['RadFil'],
+      install_requires=[
+          'matplotlib',
+          'networkx',
+          'numpy',
+          'scipy',
+          'astropy'
+      ],
+      test_suite='musca.collector',
+      tests_require=['musca'],
+      zip_safe=False)
