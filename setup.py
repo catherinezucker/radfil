eea6d2795b3935ebee7468dba1f6ea7ae2d76575
hopehhchen <hhchen@cfa.harvard.edu>
2017-09-12 13:35:01 -0400

    Update the setup files in preparation for publication.

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
-from setuptools import setup
+from distutils.core import setup
 
-setup(name='RadFil',
-      version='0.1',
-      description='Build Radial Density Profiles for Interstellar Filaments',
-      url='http://github.com/catherinezucker/RadFil',
-      author='Catherine Zucker',
-      author_email='catherine.zucker@cfa.harvard.edu',
-      packages=['RadFil'],
-      install_requires=[
-          'matplotlib',
-          'networkx',
-          'numpy',
-          'scipy',
-          'astropy',
-          'lmfit'           
-      ],
-      test_suite='musca.collector',
-      tests_require=['musca'],
-      zip_safe=False)
+setup(
+  name = 'RadFil',
+  packages = ['RadFil'],
+  version = '0.1',
+  description = 'Build radial density profiles for interstellar filaments',
+  author = ['Catherine Zucker', 'Hope Chen'],
+  author_email = ['catherine.zucker@cfa.harvard.edu', 'hhchen@cfa.harvard.edu'],
+  url = 'http://github.com/catherinezucker/RadFil', # use the URL to the github repo
+  download_url = 'http://github.com/catherinezucker/RadFil/archive/0.1.tar.gz',
+  keywords = ['astrophysics', 'radfil', 'filaments'], # arbitrary keywords
+  classifiers = [],
+  install_requires=[
+      'numpy',
+      'scipy',
+      'matplotlib',
+      'astropy',
+      'shapely',
+      'sklearn',
+      'skimage',
+      'networkx'
+  ]
+)
