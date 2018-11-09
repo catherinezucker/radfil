37c4ece172a26b9d4d0ac72c6a2a4bc489d0df2f
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2018-11-09 15:50:36 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
   name = 'radfil',
   packages = ['radfil'],
-  version = '1.1.0',
+  version = '1.1.1',
   description = 'Build radial density profiles for interstellar filaments',
   author = ['Catherine Zucker', 'Hope Chen'],
-  author_email = ['catherine.zucker@cfa.harvard.edu', 'hhchen@cfa.harvard.edu'],
+  author_email = ['catherine.zucker@cfa.harvard.edu', 'hopechen@utexas.edu'],
   url = 'http://github.com/catherinezucker/RadFil', # use the URL to the github repo
   download_url = 'https://api.github.com/repos/catherinezucker/radfil/zipball/master',
   keywords = ['astrophysics', 'radfil', 'filaments'], # arbitrary keywords
